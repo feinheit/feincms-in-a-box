10aa65674b1df450cd6079bdd42032ef4cd8b67d
Matthias Kestenholz <mk@spinlock.ch>
2014-07-24 11:21:57 +0200

    Switch from threading to multiprocessing, seems to work better with runserver code reloading

--- {{ cookiecutter.domain }}/fabfile/dev.py
+++ {{ cookiecutter.domain }}/fabfile/dev.py
@@ -1,7 +1,7 @@
 from __future__ import print_function, unicode_literals
 
+from multiprocessing import Process
 import socket
-from threading import Thread
 
 from fabric.api import task
 
@@ -10,18 +10,18 @@ from fabfile.config import local
 
 @task(default=True)
 def dev():
-    jobs = [Thread(target=fn) for fn in [
+    jobs = [Process(target=fn) for fn in [
         lambda: local('cd %(box_sass)s && grunt'),
         lambda: local('venv/bin/python -Wall manage.py runserver'),
     ]]
     try:
         socket.create_connection(('localhost', 5432), timeout=0.1).close()
     except socket.error:
-        jobs.append(Thread(target=lambda: local('postgres')))
+        jobs.append(Process(target=lambda: local('postgres')))
     try:
         socket.create_connection(('localhost', 6379), timeout=0.1).close()
     except socket.error:
-        jobs.append(Thread(target=lambda: local('redis-server')))
+        jobs.append(Process(target=lambda: local('redis-server')))
     [j.start() for j in jobs]
     [j.join() for j in jobs]
 
