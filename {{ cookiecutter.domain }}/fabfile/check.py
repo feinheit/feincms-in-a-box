1be4ab46f46cedc2deda36daa342846676b42f78
Matthias Kestenholz <mk@spinlock.ch>
2014-09-11 15:40:50 +0200

    Even better

--- {{ cookiecutter.domain }}/fabfile/check.py
+++ {{ cookiecutter.domain }}/fabfile/check.py
@@ -5,17 +5,9 @@ from fabric.api import task
 from fabfile.config import local
 
 
-_invalid_contents = [
-    (r'import i?pdb', '*.py'),
-    (r'console.log', '*.js'),
-    (r'console.log', '*.html'),
-]
-
-
 @task(default=True)
 def check():
-    for row in _invalid_contents:
-        local('! git grep -n -C3 -E \'%s\' -- \'%s\'' % row)
-
+    local("! git grep -n -C3 -E 'import i?pdb' -- '*.py'")
+    local("! git grep -n -C3 -E 'console\.log' -- '*.html' '*.js'")
     local('flake8 .')
     local('venv/bin/python manage.py check')
