210206ce96b6232b9b29ba7f06d88d59ac07f2f8
Matthias Kestenholz <mk@spinlock.ch>
2014-09-11 15:39:02 +0200

    Prepending ! to grep also negates the exit code, and is nicer

--- {{ cookiecutter.domain }}/fabfile/check.py
+++ {{ cookiecutter.domain }}/fabfile/check.py
@@ -1,7 +1,6 @@
 from __future__ import print_function, unicode_literals
 
 from fabric.api import task
-from fabric.utils import abort
 
 from fabfile.config import local
 
@@ -16,11 +15,7 @@ _invalid_contents = [
 @task(default=True)
 def check():
     for row in _invalid_contents:
-        output = local(
-            'git grep -n -C3 -E \'%s\' -- \'%s\' ; echo $?' % row,
-            capture=True)
-        if output.strip().endswith('\n0'):
-            abort('Invalid file contents, aborting.')
+        local('! git grep -n -C3 -E \'%s\' -- \'%s\'' % row)
 
     local('flake8 .')
     local('venv/bin/python manage.py check')
