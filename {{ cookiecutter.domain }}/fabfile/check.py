a9059e73c39bfd671ce52f41dc279cdade486caf
Matthias Kestenholz <mk@spinlock.ch>
2014-09-11 15:32:31 +0200

    Make "fab check" fail when pdb/ipdb is imported

--- {{ cookiecutter.domain }}/fabfile/check.py
+++ {{ cookiecutter.domain }}/fabfile/check.py
@@ -1,11 +1,24 @@
 from __future__ import print_function, unicode_literals
 
 from fabric.api import task
+from fabric.utils import abort
 
 from fabfile.config import local
 
 
+_invalid_contents = [
+    (r'import i?pdb', '*.py'),
+]
+
+
 @task(default=True)
 def check():
+    for row in _invalid_contents:
+        output = local(
+            'git grep -n -C3 -E \'%s\' -- \'%s\' ; echo $?' % row,
+            capture=True)
+        if output.strip().endswith('\n0'):
+            abort('Invalid file contents, aborting.')
+
     local('flake8 .')
     local('venv/bin/python manage.py check')
