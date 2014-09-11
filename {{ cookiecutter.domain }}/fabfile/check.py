4c88b04ce5cc848f19a968489d0f38e13f85a918
Matthias Kestenholz <mk@spinlock.ch>
2014-09-11 16:03:18 +0200

    Improve the print regex a bit to (hopefully) only catch real print() uses

--- {{ cookiecutter.domain }}/fabfile/check.py
+++ {{ cookiecutter.domain }}/fabfile/check.py
@@ -9,6 +9,6 @@ from fabfile.config import local
 def check():
     local("! git grep -n -C3 -E 'import i?pdb' -- '*.py'")
     local("! git grep -n -C3 -E 'console\.log' -- '*.html' '*.js'")
-    local("! git grep -n -C3 -E 'print' -- '%(box_project_name)s/*py'")
+    local("! git grep -n -C3 -E '(^| )print( |\(|$)' -- '%(box_project_name)s/*py'")
     local('flake8 .')
     local('venv/bin/python manage.py check')
