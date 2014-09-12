dd13531593c33cdc3c696580b9b074a318c19090
Matthias Kestenholz <mk@spinlock.ch>
2014-09-12 08:35:32 +0200

    Make it easier to run the coding style check on editable requirements

--- {{ cookiecutter.domain }}/fabfile/check.py
+++ {{ cookiecutter.domain }}/fabfile/check.py
@@ -5,12 +5,18 @@ from fabric.api import task
 from fabfile.config import local
 
 
-@task(default=True)
-def check():
+def _coding_style_check():
     local("! git grep -n -C3 -E 'import i?pdb' -- '*.py'")
     local("! git grep -n -C3 -E 'console\.log' -- '*.html' '*.js'")
     local(
         "! git grep -n -C3 -E '(^| )print( |\(|$)'"
         " -- '%(box_project_name)s/*py'")
     local('flake8 .')
+
+
+@task(default=True)
+def check():
+    _coding_style_check()
+    # with lcd('venv/src/???'):
+    #    _coding_style_check()
     local('venv/bin/python manage.py check')
