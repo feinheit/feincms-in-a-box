82231042048469b64f1141e65472388c8185bab2
Matthias Kestenholz <mk@spinlock.ch>
2014-09-12 10:11:46 +0200

    Editable requirements: The python code is not contained in the same folder as the main project...

--- {{ cookiecutter.domain }}/fabfile/check.py
+++ {{ cookiecutter.domain }}/fabfile/check.py
@@ -1,22 +1,22 @@
 from __future__ import print_function, unicode_literals
 
-from fabric.api import task
+from fabric.api import env, lcd, task
 
 from fabfile.config import local
 
 
-def _coding_style_check():
-    local("! git grep -n -C3 -E 'import i?pdb' -- '*.py'")
-    local("! git grep -n -C3 -E 'console\.log' -- '*.html' '*.js'")
-    local(
-        "! git grep -n -C3 -E '(^| )print( |\(|$)'"
-        " -- '%(box_project_name)s/*py'")
-    local('flake8 .')
+def _coding_style_check(base, project_name):
+    with lcd(base):
+        local("! git grep -n -C3 -E 'import i?pdb' -- '*.py'")
+        local("! git grep -n -C3 -E 'console\.log' -- '*.html' '*.js'")
+        local(
+            "! git grep -n -C3 -E '(^| )print( |\(|$)'"
+            " -- '%s/*py'" % project_name)
+        local('flake8 .')
 
 
 @task(default=True)
 def check():
-    _coding_style_check()
-    # with lcd('venv/src/???'):
-    #    _coding_style_check()
+    _coding_style_check('.', env.box_project_name)
+    # _coding_style_check('venv/src/???', '???')
     local('venv/bin/python manage.py check')
