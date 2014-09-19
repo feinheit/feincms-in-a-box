8848672098b9accfd942edb4431b91a75d9ad771
Matthias Kestenholz <mk@spinlock.ch>
2014-09-19 10:29:33 +0200

    Add documentation stubs

---
+++ docs/conf.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+project = 'cookiecutter-box'
+copyright = '2014, FEINHEIT'
+version = '0.0.1'
+release = '0.0.1'
+exclude_patterns = ['_build']
+pygments_style = 'sphinx'
+html_theme = 'default'
+# html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,18 @@
+================
+cookiecutter-box
+================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
