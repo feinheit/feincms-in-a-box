5fd6d6b0ca55fc01e7bc245cf0cb9fb09df09b13
Matthias Kestenholz <mk@spinlock.ch>
2014-09-29 11:01:35 +0200

    Another case where MiddlewareNotUsed makes sense

--- {{ cookiecutter.domain }}/{{ cookiecutter.project_name }}/middleware.py
+++ {{ cookiecutter.domain }}/{{ cookiecutter.project_name }}/middleware.py
@@ -10,18 +10,18 @@ class ForceDomainMiddleware(object):
         if settings.DEBUG:
             raise MiddlewareNotUsed
 
+        try:
+            self.domain = settings.FORCE_DOMAIN
+        except AttributeError:
+            raise MiddlewareNotUsed
+
     def process_request(self, request):
         if request.method != 'GET':
             return
 
-        domain = getattr(settings, 'FORCE_DOMAIN', None)
-
-        if not domain:
-            return
-
-        if request.META['HTTP_HOST'] != domain:
+        if request.META['HTTP_HOST'] != self.domain:
             target = 'http%s://%s%s' % (
                 request.is_secure() and 's' or '',
-                domain,
+                self.domain,
                 request.get_full_path())
             return HttpResponsePermanentRedirect(target)
