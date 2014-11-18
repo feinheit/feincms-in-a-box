3b913bc16f88138ceb0baee9c2c2643f70c8be4c
Matthias Kestenholz <mk@spinlock.ch>
2014-11-18 21:49:09 +0100

    Install scss-lint, add some debugging

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@ set -v
 rm -rf build/example_com
 # TODO "Nice!' name" fails in npm/bower
 ./generate.py --charge example.com "Nice name"
+ls . *
 cd build/example_com
 yes | fab local.setup
 fab check.test
