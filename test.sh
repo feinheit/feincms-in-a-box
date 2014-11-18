780e39892fd3bf61d6d45bbac2f52fbeb46d4a91
Matthias Kestenholz <mk@spinlock.ch>
2014-11-18 17:48:32 +0100

    rm first

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -v
 ID=$RANDOM
+rm -rf build/test_`echo $ID`_ch
 # TODO "Nice!' name" fails in npm/bower
 ./generate.py --charge test-`echo $ID`.ch "Nice name"
 cd build/test_`echo $ID`_ch
