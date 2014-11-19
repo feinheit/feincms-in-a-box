f79eb6c6ec138c7f4ca9b754c64ec2ecb82a239a
Matthias Kestenholz <mk@spinlock.ch>
2014-11-19 17:47:30 +0100

    Specify the server on the command line for testing

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ set -e
 set -v
 rm -rf build/example_com
 # TODO "Nice!' name" fails in npm/bower
-./generate.py --charge example.com "Nice name"
+./generate.py --charge --server deploy@example.tld example.com "Nice name"
 ls . *
 cd build/example_com
 yes | fab local.setup
