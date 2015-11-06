e1c68b9202295a57e5bdf0e9fc2354ab6c95c71d
Matthias Kestenholz <mk@spinlock.ch>
2015-11-06 14:17:36 +0100

    Oops

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ set -e
 set -v
 rm -rf build/example_com
 # TODO "Nice!' name" fails in npm/bower
-./generate.py --charge --server deploy@example.tld example.com "Nice name" --python ${TRAVIS_PYTHON_VERSION:-python2}
+./generate.py --charge --server deploy@example.tld example.com "Nice name" --python "python${TRAVIS_PYTHON_VERSION:-2.7}"
 ls . *
 cd build/example_com
 yes | fab local.setup
